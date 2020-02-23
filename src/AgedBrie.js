ba87209e4107d0984c99712e913db04eaa519491
dwj <dwi@163.com>
2020-02-23 16:39:04 +0800

    创造三个特殊的子类

--- src/AgedBrie.js
+++ src/AgedBrie.js
@@ -1,3 +1,18 @@
 /**
  *  Created by daiwenjuan on 2020/2/23 16:29.
  */
+import Item from './item'
+export class AgedBrie extends Item {
+  name = 'Aged Brie'
+  sellIn
+  quality
+  constructor(name = 'Aged Brie', sellIn, quality) {
+    super()
+    this.name = name
+    this.sellIn = sellIn
+    this.quality = quality
+  }
+  isAgedBrie() {
+    return this.name === 'Aged Brie'
+  }
+}
--- src/BackstagePass.js
+++ src/BackstagePass.js
@@ -1,3 +1,13 @@
 /**
  *  Created by daiwenjuan on 2020/2/23 16:31.
  */
+export class BackstagePass {
+  name
+  sellIn
+  quality
+  constructor(name, sellIn, quality) {
+    this.name = name
+    this.sellIn = sellIn
+    this.quality = quality
+  }
+}

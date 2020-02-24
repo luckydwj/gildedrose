b28a7bbd6367666f7cbd8c034071aa0d29c42ab4
dwj <dwi@163.com>
2020-02-24 11:39:12 +0800

    fix

--- src/item.js
+++ src/item.js
@@ -12,4 +12,52 @@ export class Item {
   toString() {
     return `${this.name}, ${this.sell_in}, ${this.quality}`
   }
+  updateItem() {
+    if (
+      this.name !== 'Aged Brie' &&
+      this.name !== 'Backstage passes to a TAFKAL80ETC concert'
+    ) {
+      if (this.quality > 0) {
+        if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+          this.quality = this.quality - 1
+        }
+      }
+    } else {
+      if (this.quality < 50) {
+        this.quality = this.quality + 1
+        if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.sell_in < 11) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1
+            }
+          }
+          if (this.sell_in < 6) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1
+            }
+          }
+        }
+      }
+    }
+    if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+      this.sell_in = this.sell_in - 1
+    }
+    if (this.sell_in < 0) {
+      if (this.name !== 'Aged Brie') {
+        if (this.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.quality > 0) {
+            if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+              this.quality = this.quality - 1
+            }
+          }
+        } else {
+          this.quality = this.quality - this.quality
+        }
+      } else {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1
+        }
+      }
+    }
+  }
 }

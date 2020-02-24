25f89383512b6457f9a54a04c97c7b9c37688260
dwj <dwi@163.com>
2020-02-24 18:42:11 +0800

    子类创建

---
+++ src/aged-brie.js
@@ -0,0 +1,11 @@
+/**
+ *  Created by daiwenjuan on 2020/2/24 18:22.
+ */
+import { Item } from './item'
+
+export class AgedBrie extends Item {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name, sell_in, quality) {
+    super(name, sell_in, quality)
+  }
+}
---
+++ src/sulfuras.js
@@ -0,0 +1,11 @@
+/**
+ *  Created by daiwenjuan on 2020/2/24 18:40.
+ */
+import { Item } from './item'
+
+export class Sulfuras extends Item {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name, sell_in, quality) {
+    super(name, sell_in, quality)
+  }
+}
